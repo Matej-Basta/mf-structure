eaf6d3c77d9269696bcc08a99aa4f7aaa83ada12
matej <matej.basta@email.cz>
2024-04-27 17:58:54 +0200

    test

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import styles from "./footer.module.css";
 export default function Header() {
   return (
     <footer className={styles.footer}>
-      <h1>Footer</h1>
+      <h1>Footer Test</h1>
       <div className={styles.info}>
         <p>Address</p>
         <p>Contact information</p>
