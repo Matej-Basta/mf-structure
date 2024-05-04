d9fac8b0187ca96a6739b24b6ea3c19912bfc9d7
matej <matej.basta@email.cz>
2024-05-04 16:53:29 +0200

    adding staging URLs

--- next.config.mjs
+++ next.config.mjs
@@ -3,12 +3,13 @@
 import NextFederationPlugin from "@module-federation/nextjs-mf";
 
 const isProduction = process.env.NODE_ENV === 'production';
+const isPreview = process.env.VERCEL_ENV === 'preview';
 
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   webpack: (config, options) => {
-    config.output.publicPath = isProduction ?  'https://structure-dusky.vercel.app/_next/' : 'http://localhost:3005/_next/';
+    config.output.publicPath = isPreview ? 'https://structure-staging.vercel.app/_next/' : (isProduction ?  'https://structure-dusky.vercel.app/_next/' : 'http://localhost:3005/_next/');
     config.plugins.push(
       new NextFederationPlugin({
         name: "structure",
@@ -18,7 +19,7 @@ const nextConfig = {
            './Footer': "./src/components/Footer.tsx",
         },
         remotes: {
-          cart: isProduction ?  'cart@https://mf-cart.vercel.app/remoteEntry.js' : `cart@http://localhost:3009/remoteEntry.js`,
+          cart: isPreview ? 'cart@https://cart-staging.vercel.app/remoteEntry.js' : (isProduction ?  'cart@https://mf-cart.vercel.app/remoteEntry.js' : `cart@http://localhost:3009/remoteEntry.js`),
         },
         shared: {
           react: { singleton: true, requiredVersion: false },
